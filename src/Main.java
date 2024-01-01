69e25a7bb1359d2656317cb02801550090699041
Tony R <tony.ratley430@gmail.com>
2024-01-01 08:15:46 -0600

    Working Sum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Enter the two numbers for addition:");
+        Scanner readInput = new Scanner(System.in);
+        int a = readInput.nextInt();
+        int b = readInput.nextInt();
+        readInput.close();
+
+        System.out.println("The sumb of a and b is= " +Integer.sum(a,b));
     }
 
 }

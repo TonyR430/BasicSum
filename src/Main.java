5788c05789705f91b00aabdce1cec091e5116d5d
tony-ubuntu <tony.ratley430@gmail.com>
2024-01-02 06:31:49 -0600

    Working Sum

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Enter the two numbers for addition:");
         Scanner readInput = new Scanner(System.in);
-        int a = readInput.nextInt();
-        int b = readInput.nextInt();
+        int x = readInput.nextInt();
+        int y= readInput.nextInt();
         readInput.close();
 
-        System.out.println("The sum of a and b is= " +Integer.sum(a,b));
+        System.out.println("The sum of a and b is= " +Integer.sum(x,y));
+        System.out.println(x + " minus " + y + (" equals ") + (x-y));
     }
 
 }
